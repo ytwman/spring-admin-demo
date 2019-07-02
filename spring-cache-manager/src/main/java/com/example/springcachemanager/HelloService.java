452b516efeeb0abbbad584aac5de6d2eca46adf4
忽忽 <896977141@qq.com>
2019-07-03 01:37:15 +0800

    cloud

--- spring-cache-manager/src/main/java/com/example/springcachemanager/HelloService.java
+++ spring-cache-manager/src/main/java/com/example/springcachemanager/HelloService.java
@@ -1,16 +1,25 @@
 package com.example.springcachemanager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
  * @author hank (hank@meiyibao.com)
  */
+@Slf4j
 @Service
 public class HelloService {
 
     @Cacheable("cache1")
     public String sayHello(String name) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("未进入 cache...");
+        }
+
+        log.info("not in cache...");
+
         return "hello, " + name;
     }
 }
