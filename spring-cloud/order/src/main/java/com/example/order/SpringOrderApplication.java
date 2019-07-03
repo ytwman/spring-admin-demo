12634364b021616f2f138b8e0455a8490b09913b
忽忽 <896977141@qq.com>
2019-07-03 20:53:05 +0800

    cloud

--- spring-cloud/order/src/main/java/com/example/order/SpringOrderApplication.java
+++ spring-cloud/order/src/main/java/com/example/order/SpringOrderApplication.java
@@ -2,12 +2,11 @@ package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hank (hank@meiyibao.com)
  */
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class SpringOrderApplication {
     public static void main(String[] args) {
